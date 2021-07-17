d72fcc6b7b5a98b5d7b9d26f642008ec4b538909
Ville <visavola@gmail.com>
2021-07-17 21:29:26 +0300

    build added

---
+++ build/precache-manifest.f8c638bba992d6c638f1cc5c9c801856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d53fe98873e4ea10913c40aee4565f09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d334e348285235e6fa1",
+    "url": "/static/css/main.b2a17636.chunk.css"
+  },
+  {
+    "revision": "95dc697f1494aac99cc2",
+    "url": "/static/js/2.295ab629.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.295ab629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d334e348285235e6fa1",
+    "url": "/static/js/main.12e955c3.chunk.js"
+  },
+  {
+    "revision": "d00a55e58aad67ff89c6",
+    "url": "/static/js/runtime-main.01dd18a1.js"
+  }
+]);
